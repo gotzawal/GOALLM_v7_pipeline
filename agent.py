22072d87d5932dcd226332ad76cebe29708c58ff
sla <zerotypepeer@gmail.com>
2025-04-16 14:08:03 +0900

    Update agent.py

--- agent.py
+++ agent.py
@@ -62,15 +62,15 @@ class GPTagent:
             "You will be provided with the agent's observation, what it carries, and the plan it follows.\n"
             "Your task is to extract entities from this data that can later be used to query the agent's memory module to find relevant information to solve the task.\n"
             "Assign a relevance score from 1 to 2 to every entity, reflecting the importance of the entity and potential memories connected to it for the current plan and goals of the agent.\n"
-            "Do not extract items like 'west', 'east', 'east exit', 'south exit'.\n"
+            "Do not extract multiple items in one sentense like 'player and npc'.\n"
             "Pay attention to the main goal of the plan.\n"
-            "**IMPORTANT** Ensure that all extracted entities are in English.\n\n"
+            "**IMPORTANT** Ensure that all extracted entities are in English.(Even it's name or written in other language)\n\n"
             "Current observation: {}\n".format(observation) +
             "Current plan: {}\n\n".format(plan) +
             "Answer in the following format:\n"
             '{"entity_1": score1, "entity_2": score2, ...}\n'
             "Do not write anything else\n"
         )
-        response, cost = self.generate(prompt)
+        response, cost = self.generate(prompt, t=0.1)
         entities_dict = ast.literal_eval(response)
         return entities_dict, cost
